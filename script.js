f3b042360a05bac15127c803f0bc3a7f7c39421a
bw12wb <35078199+bw12wb@users.noreply.github.com>
2018-01-14 21:39:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function goToAuthorsPage() {
+    window.location.assign('authors.html');
+}
+
+function startNewGame() {
+    window.location.assign('newgame.html');
+}
+
+function goToZasadyPage() {
+    window.location.assign('zasady.html');
+}
+
+function goToLogowaniePage() {
+    window.location.assign('logowanie.html');
+}
+function goToUstawieniaPage(){
+	window.location.assign('ustawienia.html');
+}
+function goToGuestPage(){
+	window.location.assign('guest.html');
+}
+function goToZalogowanyGraczGra(){
+	window.location.assign('zalogowanygraczgra.html')
+}
+function goToNajlepszewyniki(){
+	window.location.assign('najlepszewyniki.html')
+}
+function goToKategorie(){
+	window.location.assign('kategorie.html')
+}
